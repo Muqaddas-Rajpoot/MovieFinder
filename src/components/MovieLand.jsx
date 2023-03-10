519bfe13912a823bb7243ae19c7c6c9d708a2056
Muqaddas Rizwan <muqadascreator@gmail.com>
2023-03-10 06:57:21 +0500

    monvie finder app

---
+++ src/components/MovieLand.jsx
@@ -0,0 +1,51 @@
+
+import React from "react";
+import { useState, useEffect } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+const API_URL = "http://www.omdbapi.com?apikey=ebbb69c2";
+export const MovieLand = () => {
+
+  const [movies, setMovies] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <>
+      <h1>MovieLand </h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies by Title"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </>
+  );
+};
